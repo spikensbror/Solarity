28c1bbb3bdd4b76c8e88f40cabf4ad5e926df56c
Kimmy Andersson <cyaspik@gmail.com>
2011-06-26 18:38:30 +0000

    Added rendering functionality and mustache integration to the View class.

--- core/view.php
+++ core/view.php
@@ -12,6 +12,38 @@
 
 class View
 {
+    private $_tags = array();
+    
+    function __construct()
+    {
+        Solarity::get_instance()->load_library('mustache');
+        
+        $this->add_tags
+        (
+            array
+            (
+                'APP_URL' => APP_URL
+            );
+        );
+    }
+    
+    public function add_tags($tags)
+    {
+        $this->_tags = array_merge($this->_tags, $tags);
+    }
+    
+    public function render($view, $tags = false, $extras = true)
+    {
+        $path = APP_ROOT . 'views/' . $view . '.php';
+        if(!file_exists($path))
+        {
+            return false;
+        }
+        
+        ($extras) ? $this->render('core/header', false, false) : null;
+        echo(Solarity::get_instance()->mustache->render($view, (!$tags) ? $this->_tags : array_merge($this->_tags, $tags)));
+        ($extras) ? $this->render('core/header', false, false) : null;
+    }
 }
 
 ?>
