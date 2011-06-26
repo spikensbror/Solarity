5f86583eeab48ab8cf75c4496dd0f327a0c607c0
Kimmy Andersson <cyaspik@gmail.com>
2011-06-26 16:25:56 +0000

    Mustache MVC Wrapper

---
+++ libraries/mustache/controller.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+
+* Solarity
+* An all-purpose PHP framework.
+
+* Author: Kimmy Andersson
+* Website: http://www.solar-designworks.com/
+
+* Mustache MVC Wrapper
+
+**/
+
+class Mustache extends Controller
+{
+    function __construct()
+    {
+        parent::__construct('MustacheModel', false);
+    }
+    
+    public function render($path, $tags, $json = false)
+    {
+        echo($this->_model->render($path, ($json) ? json_decode($tags) : $tags));
+    }
+}
+
+?>
